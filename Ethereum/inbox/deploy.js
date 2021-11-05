702dfecf459cdb42ccc746899a047fe74e38788e
jasonj85 <jasonj85@gmail.com>
2021-11-05 17:23:04 +0000

    deploy smart contract using hdwalletprovider

--- Ethereum/inbox/deploy.js
+++ Ethereum/inbox/deploy.js
@@ -1,1 +1,29 @@
-// deploy code will go here
+const HdWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+// development only account. No money here lol!
+const provider = new HdWalletProvider(
+  "rumble degree toward target wet garden old female father suspect pilot result",
+  "https://rinkeby.infura.io/v3/1ffa3ece1b524421aaf8f4da341730b4"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Deploying from account: ", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Yo bro!"],
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Deployed to: ", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
--- Ethereum/inbox/test/Inbox.test.js
+++ Ethereum/inbox/test/Inbox.test.js
@@ -4,6 +4,7 @@ const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require("../compile");
 
+const INITIAL_MESSAGE = "Hello mate!";
 let accounts;
 let inbox;
 
@@ -15,13 +16,26 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hello!"],
+      arguments: [INITIAL_MESSAGE],
     })
     .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
   it("deploys a contract", () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("Bonjour!").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, "Bonjour!");
   });
 });
